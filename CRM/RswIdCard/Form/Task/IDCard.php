8bfdd26c6d35a59ad80d361ac3c4ae817d5611c4
andrewpthompson <andrewpthompson@users.noreply.github.com>
2018-12-19 16:51:38 +1030

    Fix to allow no logo file

--- CRM/RswIdCard/Form/Task/IDCard.php
+++ CRM/RswIdCard/Form/Task/IDCard.php
@@ -459,7 +459,9 @@ class CRM_RswIdCard_Form_Task_IDCard extends CRM_Contact_Form_Task {
       $this->pdf->writeHTMLCell($col1Width, $textRSWHeight, $x + $this->pdf->paddingLeft, $this->pdf->paddingTop + $logoHeight + $y, $titleText);
     }
 
-    $this->pdf->Image($logoFile, $x + $this->pdf->paddingLeft + ($col1Width - $logoWidth) / 2, $y + $this->pdf->paddingTop, $logoWidth, $logoHeight, $type = '', $link = '', $align = '', $resize = false, $dpi = 300, $palign = '', $ismask = false, $imgmask = false, $border = 0, $fitbox = true, $hidden = false, $fitonpage = false, $alt = false, $altimgs = array());
+    if ($logofile) {
+      $this->pdf->Image($logoFile, $x + $this->pdf->paddingLeft + ($col1Width - $logoWidth) / 2, $y + $this->pdf->paddingTop, $logoWidth, $logoHeight, $type = '', $link = '', $align = '', $resize = false, $dpi = 300, $palign = '', $ismask = false, $imgmask = false, $border = 0, $fitbox = true, $hidden = false, $fitonpage = false, $alt = false, $altimgs = array());
+    }
 
     if ($photoFile) {
       // If the contact image is not of the correct aspect ratio it will be cropped using a clipping mask in TCPDF
