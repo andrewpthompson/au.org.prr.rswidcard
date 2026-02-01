795c7bc98d80570cb5ebdcc63b4d2f7837c4a2ab
andrewpthompson <andrewpthompson@users.noreply.github.com>
2026-02-01 22:29:35 +1030

    Remove from repo
    Replaced with index.dist.php

--- rsw/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-// This file can be used to provide a shorter URL by placing it near the top of the webroot, e.g. <webroot>/rsw.
-// Copy index.dist.php to index.php, and edit the below to the correct path of au.org.prr.rswidcard/extern/rswdata.php
-// in the CiviCRM extdirectory.
-// e.g. equire_once '/var/www/joomlasubdirectory/media/civicrm/ext/au.org.prr.rswidcard/extern/rswdata.php';
-
-require_once '../membership/media/civicrm/ext/au.org.prr.rswidcard/extern/rswdata.php';
