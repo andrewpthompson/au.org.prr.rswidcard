4234563c696e891c87f2b359f0a2c4e991ad5617
andrewpthompson <andrewpthompson@users.noreply.github.com>
2026-02-01 22:29:35 +1030

    Remove from repo
    Replaced with index.dist.php

--- rsw/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-//require_once '../membership/media/civicrm/ext/au.org.prr.rswidcard/extern/rswdata.php';
-require_once '../wp-content/uploads/civicrm/ext/au.org.prr.rswidcard/extern/rswdata.php';
-
-?>
